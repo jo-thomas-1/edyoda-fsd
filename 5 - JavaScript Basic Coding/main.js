7234cafef40b24a722d74aa1a4b85d3280a99a70
Jo Thomas <jothomas1996@gmail.com>
2022-06-05 22:15:55 +0530

    two assignment questions answered

--- 5 - JavaScript Basic Coding/main.js
+++ 5 - JavaScript Basic Coding/main.js
@@ -1,1 +1,22 @@
-console.log("Hello World");
+console.clear();
+
+// Answer 1
+
+var str1 = 'Today is';
+var str2 = '      a beautiful day     ';
+var str3 = ' In Hawaii.     ';
+
+// Result = 'Today is a beautiful day In Hawaii.'
+// Use the str1, str2, str3 variables to create the Result string. Keep the extra spaces, lowercase and uppercase letters in mind.
+
+var result = str1.trim() + " " + str2.trim() + " " + str3.trim();
+
+console.log("Result: " + result);
+
+//-----------------------------------------------------------------------------------
+
+// Answer 2
+
+var enteredAlphabet = 'd' or "D"
+
+// Write a program to check vowel, consonant or non-alphabet using switch case. Change value of enteredAlphabet and check if your code works fine.

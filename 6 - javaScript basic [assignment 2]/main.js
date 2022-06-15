e190ae4997ff08d83af655c5aa011d0b708d1596
Jo Thomas <jothomas1996@gmail.com>
2022-06-15 22:44:31 +0530

    completed questions in js assig 2

--- 6 - javaScript basic [assignment 2]/main.js
+++ 6 - javaScript basic [assignment 2]/main.js
@@ -37,27 +37,77 @@ console.log("Answer 2 -----------------------------------");
 // 54
 // 5
 
-function pattern_1(argument)
+function pattern_1(value)
 {
-	// body...
+	for(var i = 0 ; i < value ; i++)
+	{
+		var temp = "";
+		for(var j = value ; j > i ; j--)
+		{
+			temp = temp + j;
+		}
+		console.log(temp);
+	}
 }
+
+pattern_1(8);
 
 //===================================================================================
 
 // Answer 3
 console.log("Answer 3 -----------------------------------");
 
+// Write a JavaScript program to sort an array in ascending order
 
+function sort_asscending(array)
+{
+	array.sort(function(a, b) { return a - b; });
+	return array;
+}
+
+console.log(sort_asscending([3, 2, 1, 4, 5, 45]));
 
 //===================================================================================
 
 // Answer 4
 console.log("Answer 4 -----------------------------------");
 
+// Write a JavaScript program to check if two strings are anagrams.
+// Anagram string are such two strings which can be rearranged to create one another.
 
+function anagram(str_1, str_2)
+{
+	if(str_1.length == str_2.length)
+	{
+		for(var i = 0 ; i < anagram.length ; i++)
+		{
+			if(!str_2.includes(str_1[i])) return false;
+		}
+
+		return true;
+	}
+	return false;
+}
+
+if(anagram("HELLO", "JELLO")) console.log("True");
+else console.log("False");
 
 //===================================================================================
 
 // Answer 5
 console.log("Answer 5 -----------------------------------");
 
+// Write a JavaScript program to reverse a string without using inbuilt methods.
+
+function reverse_string(str)
+{
+	var temp = "";
+	for(var i = str.length - 1 ; i >= 0 ; i-- )
+	{
+		temp = temp + str[i];
+	}
+
+	return temp;
+}
+
+console.log(reverse_string("Hello"));
